e86f5a27b0362c48c960518a76149cd6e3aa6cc4
Samuel K. Grush <Samuel@Grush.org>
2017-09-24 20:13:00 -0600

    Improved __main__

--- __main__.py
+++ __main__.py
@@ -6,17 +6,26 @@ import os
 import os.path
 import logging
 
+__DIR__ = os.path.dirname(os.path.abspath(__file__))
+
 try:
     import win10py3lcd2usb
     win10py3lcd2usb.init()
 except ImportError:
     if 'win10py3lcd2usb' in sys.argv:
         import logging.config
-        d = os.path.dirname(os.path.abspath(__file__))
-        logging.config.fileConfig(os.path.join(d, "setup.cfg"))
+        logging.config.fileConfig(os.path.join(__DIR__, "setup.cfg"))
         m = "You called me wrong; should've used `py -m win10py3lcd2usb`"
         print(m)
         logging.getLogger().critical(m)
+    else:
+        _cwd = os.getcwd()
+        try:
+            os.chdir(os.path.join(__DIR__, '..'))
+            import win10py3lcd2usb
+            win10py3lcd2usb.init()
+        finally:
+            os.chdir(_cwd)
     raise
 
 # Pseudo-local imports
